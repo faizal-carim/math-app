27f926d0f09fc9c6f339aa01232acc6b02c14c71
Abdool Carim <Faizal.AbdoolCarim@standardbank.co.za>
2025-05-23 10:45:59 +0200

    added some parameters for hosting

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/math-game-app/',
   plugins: [react()],
   server: {
     proxy: {
