e8910d0fc0c5dde16b01c87246480a106fcb964d
Abdool Carim <Faizal.AbdoolCarim@standardbank.co.za>
2025-05-26 11:23:49 +0200

    fixed the config and render issue

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const API_URL = 'https://math-app-w36c.onrender.com';
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://math-game-api.onrender.com' 
+  : 'http://localhost:3001';
 
 export default API_URL;
