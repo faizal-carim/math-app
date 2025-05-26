cd91de3e8c0accd0aeb7f11f2ecfc0ead6d549f5
Abdool Carim <Faizal.AbdoolCarim@standardbank.co.za>
2025-05-26 11:35:49 +0200

    fixing cors issue

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,12 @@ const adminRoutes = require("./routes/admin");
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://math-game-client.onrender.com', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
